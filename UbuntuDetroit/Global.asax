e58a1f1b901b82e8ab0766c0529aac093344d67b
Darius Hunter <darius777hunter@gmail.com>
2016-11-30 15:19:18 -0500

    First Step creation
    Still need to customize and add the database

---
+++ UbuntuDetroit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UbuntuDetroit.MvcApplication" Language="C#" %>
---
+++ UbuntuDetroit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UbuntuDetroit.Startup))]
+namespace UbuntuDetroit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
